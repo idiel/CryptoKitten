67dae3674fa685466e71fc85fcf1a6d0d592fcc1
Sean Park <sean.2015@kakaocorp.com>
2018-06-08 16:26:40 +0900

    CryptoKitten package file update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
     	.library(
     		name: "CryptoKitten",
-    		targets: ["CryptoKitten"])
+    		targets: ["CryptoKitten"]
     	)
     ],
     targets: [
